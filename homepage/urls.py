151c52d193f21b828cef4677fcbab6167bd00a2c
Vlad Bednyakov <vladik199817@gmail.com>
2023-09-09 03:01:43 +0300

    init

---
+++ homepage/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', index, name='home'),        #http://127.0.0.1:8000/homepage/
+    path('qwe/', qwe, name='qwe'),      #http://127.0.0.1:8000/homepage/qwe/
+    path('docker/', docker, name='docker'),
+    path('systemd/', systemd, name='systemd'),
+    path('samba/', samba, name='samba'),
+    path('disk/', disk, name='disk'),
+    path('swap-linux/', swap_linux, name='swap_linux'),
+    path('firewalld/', firewalld, name='firewalld'),
+    path('cisco/', cisco, name='cisco'),
+    path('postgresql/', postgresql, name='postgresql'),
+    path('netsh/', netsh, name='netsh'),
+    path('haproxy/', haproxy, name='haproxy'),
+    path('ufw/', ufw, name='ufw'),
+    path('git/', git, name='git'),
+]
---
+++ homepage/views.py
@@ -0,0 +1,59 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+
+# Create your views here.
+menu = ["Этот", "список", "вызывается", "в", "файле" ,"/homepage/views.py"]
+
+
+def index(request):
+    #return HttpResponse("<h1>This is HomePage!</h1>")
+    return render(request, 'homepage/index.html', {'menu': menu, 'title': 'Главная страница'})
+
+def qwerty(request):
+    return HttpResponse("<h1>QWERTY!</h1>")
+
+def qwe(request):
+    #return HttpResponse("<h1>This is HomePage!</h1>")
+    return render(request, 'homepage/qwe.html')
+
+def docker(request):
+    return render(request, 'homepage/docker.html', {'title': 'Docker'})
+
+def systemd(request):
+    return render(request, 'homepage/systemd.html', {'title': 'Systemd'})
+
+def samba(request):
+    return render(request, 'homepage/samba.html', {'title': 'Samba'})
+
+def disk(request):
+    return render(request, 'homepage/disk.html', {'title': 'LV'})
+
+def swap_linux(request):
+    return render(request, 'homepage/swap_linux.html', {'title': 'Swap Linux'})
+
+def firewalld(request):
+    return render(request, 'homepage/firewalld.html', {'title': 'firewalld'})
+
+def cisco(request):
+    return render(request, 'homepage/cisco.html', {'title': 'Cisco'})
+
+
+def postgresql(request):
+    return render(request, 'homepage/postgresql.html', {'title': 'PostgreSQL'})
+
+
+def netsh(request):
+    return render(request, 'homepage/netsh.html', {'title': 'Netsh'})
+
+def haproxy(request):
+    return render(request, 'homepage/haproxy.html', {'title': 'HAProxy'})
+
+def ufw(request):
+    return render(request, 'homepage/ufw.html', {'title': 'ufw'})
+
+def git(request):
+    return render(request, 'homepage/git.html', {'title': 'git'})
+
+def PackageNotFoundError(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
